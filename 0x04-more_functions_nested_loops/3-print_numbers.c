c74ccbc3839a8a99a429d37b1cf8e9b5d17f5d55
Richcandt <kwizerarichard27@gmail.com>
2023-07-20 14:10:01 -0700

    The numbers spea

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0 to 9
- * Return:Always 0
- */
-
+  * print_numbers - Print the numbers since 0 up to 9
+  *
+  * Return: The numbers since 0 up to 9
+  */
 void print_numbers(void)
 {
-char c;
+	int x;
 
-for (c = '0'; c <= '9'; c++)
-{
-putchar(c);
-}
-putchar('\n');
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+
+	_putchar('\n');
 }

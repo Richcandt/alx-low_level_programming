ed9d68685b9501215e2930d1e5960a79310c093d
Richcandt <kwizerarichard27@gmail.com>
2023-10-02 09:03:54 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,45 +1,41 @@
 #include "main.h"
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * append_text_to_file - function that appends text at the end of a file
- *
- * @filename: name of the file
- * @text_content: NULL terminated string to append to end of file
- *
- * Return: Returns: 1 on success, -1 on failure
- * Do not create the file if it does not exit
- *
- * if filename is NULL return -1
- * If text_content is NULL, do not add anything to the file.
- * Return 1 if the file exists and -1 if the file does not exist or if you
- * do not have the required permissions to write the file
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0
  */
-int append_text_to_file(const char *filename, char *text_content)
+
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, checkw, l = 0;
+	ssize_t file, let, g;
+	char *text;
 
-	if (filename == 0)
-		return (-1);
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	if (filename == NULL)
+		return (0);
 
-	if (fd == -1)
-		return (-1);
+	file = open(filename, O_RDONLY);
 
-	if (text_content)
+	if (file == -1)
 	{
-		while (text_content[l] != 0)
-			l++;
-
-		checkw = write(fd, text_content, l);
-
-		if (checkw == -1)
-			return (-1);
+		free(text);
+		return (0);
 	}
 
-	close(fd);
-	return (1);
+	let = read(file, text, letters);
+
+	g = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (g);
 }

ce97aa5981f7eee5c23af374efabc073a3cb2a14
Richcandt <kwizerarichard27@gmail.com>
2023-10-03 05:23:00 -0700

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,10 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <unist.h>
 #include <sys/types.h>
-
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
